29aa1c838df52c7b95cfd184e073e31703d55a66
Vebjørn Bergsholm Bjørhovde <vb.bjorhovde@gmail.com>
2021-03-05 17:13:42 +0100

    Moved velocity fix from the plotter to a separate function that will generate a new csv file with updated velocities

--- data_plotter.py
+++ data_plotter.py
@@ -21,8 +21,6 @@ def data_plotter(filename: str, joint: int, total_time: int) -> None:
 def data_plotter_rt(filename: str, joint: int) -> None:
     commands = []
     feedback = []
-    current_pos = 0
-    previous_pos = 0
     total_lines = 0
 
     with open(filename) as csv_file:
@@ -30,15 +28,8 @@ def data_plotter_rt(filename: str, joint: int) -> None:
         
         for line in csv_reader:
             line = line[0].split('\t')
-            current_pos = float(line[joint+20])
             commands.append(float(line[joint+10]))
-
-            if current_pos < previous_pos:
-                feedback.append(-float(line[joint]))
-            else:
-                feedback.append(float(line[joint]))
-            
-            previous_pos = current_pos
+            feedback.append(float(line[joint]))
 
             total_lines = csv_reader.line_num
 
@@ -48,4 +39,25 @@ def data_plotter_rt(filename: str, joint: int) -> None:
     plt.legend(loc='upper right')
     plt.grid(True)
     plt.show()
+
+def fix_velocity_sign(filename: str) -> None:
+    name_components = filename.split('.')
+    new_filename = name_components[0] + '_fixed.' + name_components[1]
+
+    current_pos = [0.0]*10
+    previous_pos = [0.0]*10
+
+    with open(filename, 'r') as raw_data:
+        csv_reader =csv.reader(raw_data)
+        with open(new_filename, 'w') as fixed_data:
+            csv_writer = csv.writer(fixed_data, delimiter='\t')
+            for line in csv_reader:
+                line = line [0].split('\t')
+                current_pos = line[20:29]
+                for i in range(len(current_pos)):
+                    if float(current_pos[i]) < float(previous_pos[i]):
+                        line[i] = "-" + line[i]
+                csv_writer.writerow(line)
+                previous_pos = current_pos   
+
     
