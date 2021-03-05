93801fed322f3107e29a4dae2b38669305087a29
Vebjørn Bergsholm Bjørhovde <vb.bjorhovde@gmail.com>
2021-03-05 15:03:56 +0100

    Added plotter for rt data

--- data_plotter.py
+++ data_plotter.py
@@ -2,8 +2,8 @@ import csv
 import numpy as np
 from matplotlib import pyplot as plt
 
+#Plots data from motion_log.csv
 def data_plotter(filename: str, joint: int, total_time: int) -> None:
-    
     data = []
     with open(filename, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -15,5 +15,25 @@ def data_plotter(filename: str, joint: int, total_time: int) -> None:
     time_list = np.linspace(0,total_time, len(data))
 
     plt.plot(time_list, data)
-    plt.show()       
+    plt.show()
 
+#Plots data from motion_log_rt.csv
+def data_plotter_rt(filename: str, joint: int) -> None:
+    commands = []
+    feedback = []
+    total_lines = 0
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        
+        for line in csv_reader:
+            line = line[0].split('\t')
+            commands.append(float(line[joint+10]))
+            feedback.append(float(line[joint]))
+            total_lines = csv_reader.line_num
+
+    cycle_list = np.linspace(0, total_lines, total_lines)
+    plt.plot(cycle_list, commands, label='Command')
+    plt.plot(cycle_list, feedback, label='Feedback')
+    plt.legend(loc='upper right')
+    plt.grid(True)
+    plt.show()
