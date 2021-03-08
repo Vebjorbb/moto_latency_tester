12949b57735350911c745bcd2f6c30d3e2d2f245
Vebjørn Bergsholm Bjørhovde <vb.bjorhovde@gmail.com>
2021-03-08 13:19:29 +0100

    Expanded latency calculation to return a list of average latency for each joint.

--- data_plotter.py
+++ data_plotter.py
@@ -67,6 +67,8 @@ def calculate_latency(filename: str):
     latencies = [0.0]*10
     commands = []
     feedbacks = []
+
+    #Reads data from csv-file and saves it in lists
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
@@ -76,6 +78,7 @@ def calculate_latency(filename: str):
             commands.append(line[10:20])
             feedbacks.append(line[0:10])
     
+    #Convert from lists of strings to lits of floats
     for command in commands:
         for i in range(len(command)):
             command[i] = float(command[i])
@@ -84,53 +87,54 @@ def calculate_latency(filename: str):
         for i in range(len(feedback)):
             feedback[i] = float(feedback[i])
 
-    
-    latency_list = []
+    #An average latency is calculated for each joint
+    for joint in range(len(latencies)):
+        latency_list = []
+        for i  in range(100, 1100):
+            current_command = commands[i][joint]
+            previous_command = commands[i-1][joint]
 
-    for i  in range(100, 1100):
-        current_command = commands[i][0]
-        previous_command = commands[i-1][0]
-
-        rising_command = 0
-        if current_command > previous_command:
-            rising_command = 1
-        else:
+            #Checks if the command velocity is increasing or decreasing
             rising_command = 0
+            if current_command > previous_command:
+                rising_command = 1
+            else:
+                rising_command = 0
 
 
-        current_feedback = 0
-        previous_feedback = 0
+            current_feedback = 0
+            previous_feedback = 0
+            rising_feedback = 0
 
-        latency_counter = 0 
-        rising_feedback = 0
-        for j in range(i, len(feedbacks)):
-            current_feedback = feedbacks[j][0]
-            previous_feedback = feedbacks[j-1][0]
+            latency_counter = 0 
             
-            if current_feedback > previous_feedback:
-                rising_feedback = 1
-            else:
-                rising_feedback = 0
+            for j in range(i, len(feedbacks)):
+                current_feedback = feedbacks[j][joint]
+                previous_feedback = feedbacks[j-1][joint]
+                
+                #Checks if the feedback-velocity is increasing or decreasing
+                if current_feedback > previous_feedback:
+                    rising_feedback = 1
+                else:
+                    rising_feedback = 0
 
-
-            if current_command > previous_feedback and current_command < current_feedback and rising_feedback == rising_command:
-                latency_list.append(latency_counter)
-                print('Hello 1, {}'.format(latency_counter))
-                break
-            elif current_command < previous_feedback and current_command > current_feedback and rising_feedback == rising_command:
-                latency_list.append(latency_counter)
-                print('hello 2, {}'.format(latency_counter))
-                break
-            else:
-                latency_counter += 1
-        
-        
-    latencies[0] = sum(latency_list)/len(latency_list)
+                #Compares feedback signal to command signal to determine latency
+                if current_command > previous_feedback and current_command < current_feedback and rising_feedback == rising_command:
+                    latency_list.append(latency_counter)
+                    break
+                elif current_command < previous_feedback and current_command > current_feedback and rising_feedback == rising_command:
+                    latency_list.append(latency_counter)
+                    break
+                elif current_command == current_feedback:
+                    latency_list.append(latency_counter)
+                    break
+                else:
+                    latency_counter += 1
+            
+        #Calculate average latency and save it in a list    
+        latencies[joint] = sum(latency_list)/len(latency_list)
     
     return(latencies)
 
         
-
-
-
 print(calculate_latency('motion_log_rt_ex_fixed.csv'))
