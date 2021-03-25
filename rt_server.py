35046934f6c432c605deb34eb4c9f231c5ef0614
Vebjørn Bergsholm Bjørhovde <vb.bjorhovde@gmail.com>
2021-03-25 10:08:35 +0100

    Addded csv logging

--- rt_server.py
+++ rt_server.py
@@ -20,7 +20,8 @@ def start_udp_server(address):
 def main():
     logger = open('motion_log_rt.csv', 'w')
     csv_writer = csv.writer(logger, delimiter='\t')
-    server = start_udp_server(("192.168.255.3", 50244))
+    #server = start_udp_server(("192.168.255.3", 50244))
+    server = start_udp_server(("localhost", 50244))
     started = False
 
     t0 = time.time()
@@ -42,15 +43,15 @@ def main():
             state: MotoRealTimeMotionJointStateEx = msg.body
 
             if p0 is None:
-                p0 = copy(state.joint_state_data[1].pos)
+                p0 = copy(state.joint_state_data[0].pos)
 
-            print("state:   {}".format(state.joint_state_data[1].vel[0]))
+            print("state:   {}".format(state.joint_state_data[0].vel[0]))
         
             # pd = np.deg2rad(10)
             # Kv = 0.1
             # vd = Kv * (pd - state.joint_state_data[1].pos[0])
-
             vd  = 0.3 * (np.sin(3.0 * time.time() - t0))
+            # vd  = 0.05 * (np.sin(0.1 * time.time() - t0))
 
             #vd = 0.05
 
@@ -68,17 +69,17 @@ def main():
                     state.number_of_valid_groups,
                     [
                         MotoRealTimeMotionJointCommandExData(
-                            0, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                            0, [vd, 0.0, 0.0, 0.0, 0.0, 0.0]
                         ),
-                        MotoRealTimeMotionJointCommandExData(1, [vd, vd]),
+                        MotoRealTimeMotionJointCommandExData(1, [0, 0]),
                         
                     ],
                 ),
             )
 
             server.sendto(command_msg.to_bytes(), addr)
-            csv_writer.writerow(state.joint_state_data[1].vel + command_msg.body.joint_command_data[1].command + state.joint_state_data[1].pos)
-            
+            #csv_writer.writerow(state.joint_state_data[1].vel + command_msg.body.joint_command_data[1].command + state.joint_state_data[1].pos)
+            csv_writer.writerow(state.joint_state_data[0].vel + command_msg.body.joint_command_data[0].command + state.joint_state_data[0].pos)
 
         except socket.timeout as e:
             logging.error("Timed out!")
